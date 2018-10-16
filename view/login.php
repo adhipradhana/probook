058b2818830a49a1fa4e23205d7c1d72e3df12fd
adhipradhana <adhi.pradana.pradana@gmail.com>
2018-10-16 17:54:56 +0700

    add login html

--- view/login.php
+++ view/login.php
@@ -1,9 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Log In </title>
+	<title>Log In</title>
+	<link rel="stylesheet" type="text/css" href="CSS/register.css">
 </head>
 <body>
-	<h1> Log In </h1>
+	<div class="background">
+		<div class="container">
+			<h1> LOGIN </h1>
+			<table class="data-registration">
+				<form>
+					<tr>
+						<td class="align-right">Username</td>
+						<td><input type="text" name="username"></td>
+					</tr>
+					<tr>
+						<td class="align-right">Password</td>
+						<td><input type="password" name="password"></td>
+					</tr>
+				</form>
+			</table>
+			<span id="already"><a href="#">Don't have an account?</a></span>
+			<div class="button-register">
+				<button type="button" id="submit">LOGIN</button>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
