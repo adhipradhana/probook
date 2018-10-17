f82e2cb3bce2bd4ea282c8fed7cfe84c43a048aa
Christian Jonathan <christianchrisjo@gmail.com>
2018-10-17 21:43:15 +0700

    edited register.css

--- view/profile.php
+++ view/profile.php
@@ -8,8 +8,14 @@
 	<div class="header">
 		<span id="pro">Pro-</span><span id="book">Book</span> <span class="header2"> <span id="user">Hi, username</span> <span id="logicon"> icon log</span> </span>
 	</div>
-	<div class="header-2">
-		
+	<div class="main-section">
+		<div class="browse"><strong>B</strong>rowse</div> 
+		<div class="history"><strong>H</strong>istory</div> 
+		<div class="profile"><strong>P</strong>rofile</div>
+	</div>
+	<div class="profile-pic">
+		<span class="edit-pic">EDIT ICON</span>
+		<div class="pic-border"><span id="prof-pic">PIC</span></div>
 	</div>
 	
 </body>
