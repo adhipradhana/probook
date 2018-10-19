4d98809d2109a223132e42e4c138304546e01ad5
yusufrahmatp <yusufrahmatp@gmail.com>
2018-10-19 18:52:10 +0700

    Fix bug, wrong variable in database.php

--- model/Database.php
+++ model/Database.php
@@ -7,10 +7,10 @@ class Database {
             $conn = new PDO($dsn, $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             echo "Connected successfully";
-            return $conn
+            return $conn;
         }
         catch (PDOException $p) {
-            echo "Connection failed " . $e->getMessage();
+            echo "Connection failed " . $p->getMessage();
         }
     }
 }
