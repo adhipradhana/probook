7d707332cf13151027718f5df74974b879cc9a21
yusufrahmatp <yusufrahmatp@gmail.com>
2018-10-23 15:14:24 +0700

    Initial html and css for book detail and order page

---
+++ view/detail.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Book Detail</title>
+    <link rel="stylesheet" type="text/css" href="CSS/detail.css">
+    <link href="https://fonts.googleapis.com/css?family=Karla:400,400i,700,700i&amp;subset=latin-ext" rel="stylesheet">
+</head>
+<body>
+    <div class="header">
+        <span id="pro">Pro-</span><span id="book">Book</span> 
+        <span class="header2"> 
+            <span id="user">Hi, username</span> <span id="logicon"> icon log</span> 
+        </span>
+    </div>
+    <div class="main-section">
+        <a href="#"><div class="browse"><strong class="font2em">B</strong>rowse</div></a>
+        <a href="#"><div class="history"><strong class="font2em">H</strong>istory</div></a> 
+        <a href="#"><div class="profile"><strong class="font2em">P</strong>rofile</div></a>
+    </div>
+    <div class="book-detail">
+        <div class="book-text-desc">
+            <p id="book-title">Nota Hidup</p>
+            <p id="book-author">Light R.D.B.</p>
+            <p id="book-desc">Buku ajaib yang berisi nama-nama orang terpilih. Jika namamu tertulis di bukui ini maka kamu adalah salah satu orang yang beruntung.</p>
+        </div>
+        <div class="book-nontext-desc">
+            <p id="book-image">Logo</p>
+            <div class="book-ratings">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
