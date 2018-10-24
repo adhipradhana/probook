ed5efe09e2da60020aa89bf030862e79bac2f282
yusufrahmatp <yusufrahmatp@gmail.com>
2018-10-24 14:16:05 +0700

    Cleanup template data, finalize details page excluding header

--- view/detail.php
+++ view/detail.php
@@ -20,21 +20,21 @@
     <div class="main-page">
         <div class="book-detail">
             <div class="book-text-desc">
-                <h1 id="book-title">Nota Hidup</h1>
-                <p id="book-author">Light R.D.B.</p>
-                <p id="book-desc">Buku ajaib yang berisi nama-nama orang terpilih. Jika namamu tertulis di bukui ini maka kamu adalah salah satu orang yang beruntung.Buku ajaib yang berisi nama-nama orang terpilih. Jika namamu tertulis di bukui ini maka kamu adalah salah satu orang yang beruntung.Buku ajaib yang berisi nama-nama orang terpilih. Jika namamu tertulis di bukui ini maka kamu adalah salah satu orang yang beruntung.Buku ajaib yang berisi nama-nama orang terpilih. Jika namamu tertulis di bukui ini maka kamu adalah salah satu orang yang beruntung.Buku ajaib yang berisi nama-nama orang terpilih. Jika namamu tertulis di bukui ini maka kamu adalah salah satu orang yang beruntung.Buku ajaib yang berisi nama-nama orang terpilih. Jika namamu tertulis di bukui ini maka kamu adalah salah satu orang yang beruntung.Buku ajaib yang berisi nama-nama orang terpilih. Jika namamu tertulis di bukui ini maka kamu adalah salah satu orang yang beruntung.Buku ajaib yang berisi nama-nama orang terpilih. Jika namamu tertulis di bukui ini maka kamu adalah salah satu orang yang beruntung.Buku ajaib yang berisi nama-nama orang terpilih. Jika namamu tertulis di bukui ini maka kamu adalah salah satu orang yang beruntung.Buku ajaib yang berisi nama-nama orang terpilih. Jika namamu tertulis di bukui ini maka kamu adalah salah satu orang yang beruntung.Buku ajaib yang berisi nama-nama orang terpilih. Jika namamu tertulis di bukui ini maka kamu adalah salah satu orang yang beruntung.</p>
+                <h1 id="book-title">-</h1>
+                <p id="book-author">-</p>
+                <p id="book-desc">-</p>
             </div>
             <div class="book-nontext-desc">
-                <img id="book-image" src="asset/book.jpeg">
+                <img id="book-image" src="">
                 <div class="book-ratings">
-                    <img id="star-1" src="asset/full_star.png">
-                    <img id="star-2" src="asset/full_star.png">
-                    <img id="star-3" src="asset/full_star.png">
-                    <img id="star-4" src="asset/full_star.png">
-                    <img id="star-5" src="asset/half_star.png">
+                    <img id="star-1" src="asset/empty_star.png">
+                    <img id="star-2" src="asset/empty_star.png">
+                    <img id="star-3" src="asset/empty_star.png">
+                    <img id="star-4" src="asset/empty_star.png">
+                    <img id="star-5" src="asset/empty_star.png">
                 </div> 
                 <div class="book-ratings-num">
-                    <p class='ratings' id="book-ratings-num">4.5/5.0</p>
+                    <p class='ratings' id="book-ratings-num">-/5.0</p>
                 </div>
             </div>
         </div>
@@ -59,28 +59,6 @@
         </div>
         <div id='book-reviews-list' class="book-reviews">
             <h2>Reviews</h2>
-            <div class='review-unit'>
-                <img class='review-pic' src='asset/tayo.png'>
-                <div class='review-detail'>
-                    <h3 class='review-name'>@tayotayo</h3>
-                    <p class='review-desc'> Buku ini keren! Nama Rogi dan Lani, temanku, ada di sini, keberuntungan hebat menanti mereka ( ^_^)Buku ini keren! Nama Rogi dan Lani, temanku, ada di sini, keberuntungan hebat menanti mereka ( ^_^)Buku ini keren! Nama Rogi dan Lani, temanku, ada di sini, keberuntungan hebat menanti mereka ( ^_^)Buku ini keren! Nama Rogi dan Lani, temanku, ada di sini, keberuntungan hebat menanti mereka ( ^_^)Buku ini keren! Nama Rogi dan Lani, temanku, ada di sini, keberuntungan hebat menanti mereka ( ^_^)Buku ini keren! Nama Rogi dan Lani, temanku, ada di sini, keberuntungan hebat menanti mereka ( ^_^)Buku ini keren! Nama Rogi dan Lani, temanku, ada di sini, keberuntungan hebat menanti mereka ( ^_^)Buku ini keren! Nama Rogi dan Lani, temanku, ada di sini, keberuntungan hebat menanti mereka ( ^_^)Buku ini keren! Nama Rogi dan Lani, temanku, ada di sini, keberuntungan hebat menanti mereka ( ^_^)</p>
-                </div>
-                <div class='review-rating'>
-                    <img class='review-star' src='asset/full_star.png'>
-                    <p class='ratings'>5.0/5.0</p>
-                </div>
-            </div>
-            <div class='review-unit'>
-                <img class='review-pic' src='asset/tayo.png'>
-                <div class='review-detail'>
-                    <h3 class='review-name'>@tayotayo</h3>
-                    <p class='review-desc'> Buku ini keren! Nama Rogi dan Lani, temanku, ada di sini, keberuntungan hebat menanti mereka ( ^_^)</p>
-                </div>
-                <div class='review-rating'>
-                    <img class='review-star' src='asset/full_star.png'>
-                    <p class='ratings'>5.0/5.0</p>
-                </div>
-            </div>
         </div>
     </div>
     <div id="notification-background">
