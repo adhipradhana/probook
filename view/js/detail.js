7ceca058f51ebe930443942524a21436e70d9169
yusufrahmatp <yusufrahmatp@gmail.com>
2018-10-24 17:31:26 +0700

    Remove unnecessary variable parameter in detail js function

--- view/js/detail.js
+++ view/js/detail.js
@@ -11,7 +11,7 @@ function loadDetailData() {
     }
 
     loadUserHeader();
-    loadBookData(user_id, book_id);
+    loadBookData(book_id);
     loadReviewData(book_id);
 }
 
@@ -31,7 +31,7 @@ function loadUserHeader() {
     }
 }
 
-function loadBookData(user_id, book_id) {
+function loadBookData(book_id) {
     xhttp = new XMLHttpRequest();
     uriPath = "/controller/book_id.php?id=" + book_id;
     xhttp.open("GET", uriPath, true);
