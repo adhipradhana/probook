ee38e1e2d4c9407ed1eb02f8960ad66409c6f90d
adhipradhana <adhi.pradana.pradana@gmail.com>
2018-10-24 18:01:39 +0700

    add search result interface and functionality

--- view/search_result.php
+++ view/search_result.php
@@ -4,7 +4,7 @@
 	<link rel="stylesheet" href="/view/css/search_result.css">
 	<link href="https://fonts.googleapis.com/css?family=Karla:400,400i,700,700i&amp;subset=latin-ext" rel="stylesheet">
 </head>
-<body>
+<body onload="searchBooks();loadUserData()">
 	<div class="header">
 		<span id="pro">Pro-</span><span id="book">Book</span> 
 		<span class="header2"> 
@@ -12,12 +12,15 @@
 		</span>
 	</div>
 	<div class="main-section">
-		<a href="#"><div class="browse"><strong class="font2em">B</strong>rowse</div></a>
+		<a href="/view/search.php"><div class="browse"><strong class="font2em">B</strong>rowse</div></a>
 		<a href="#"><div class="history"><strong class="font2em">H</strong>istory</div></a> 
-		<a href="#"><div class="profile"><strong class="font2em">P</strong>rofile</div></a>
+		<a href="/view/profile.php"><div class="profile"><strong class="font2em">P</strong>rofile</div></a>
 	</div>
 	<div class="search-result-container">
-		<span id="search-result">Search Result</span>
-		<span id="search-number">Found <u>2</u> result(s)</span>
+		<div id="search-result">Search Result</div>
+		<div id="search-number">Found <u id="result-number">2</u> result(s)</div>
 	</div>
+	<div id="book-placeholder">
+	</div>
+	<script type="text/javascript" src="/view/js/search_result.js"></script>
 </body>
