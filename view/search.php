9a4f1f8f2a00b8a21078f15b86931fd52afab284
adhipradhana <adhi.pradana.pradana@gmail.com>
2018-10-24 18:42:39 +0700

    connect search page to search result

--- view/search.php
+++ view/search.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" type="text/css" href="/view/css/search.css">
 	<link href="https://fonts.googleapis.com/css?family=Karla:400,400i,700,700i&amp;subset=latin-ext" rel="stylesheet">
 </head>
-<body>
+<body onload="loadUserData()">
 	<div class="header">
 		<span id="pro">Pro-</span><span id="book">Book</span> 
 		<span class="header2"> 
@@ -14,7 +14,7 @@
 	</div>
 	<div class="main-section">
 		<a href="/view/search.php"><div class="browse"><strong class="font2em">B</strong>rowse</div></a>
-		<a href="#"><div class="history"><strong class="font2em">H</strong>istory</div></a> 
+		<a href="/view/history.php"><div class="history"><strong class="font2em">H</strong>istory</div></a> 
 		<a href="/view/profile.php"><div class="profile"><strong class="font2em">P</strong>rofile</div></a>
 	</div>
 	<div class="container">
@@ -22,7 +22,8 @@
 		<div>
 			<input type="text" id="search-box" placeholder="Input search terms...."></input>
 		</div>
-		<button id="submit" type="submit">Search</button>
+		<button id="submit" onclick="searchBook()" type="submit">Search</button>
 	</div>
+	<script type="text/javascript" src="/view/js/search.js"></script>
 </body>
 </html>
